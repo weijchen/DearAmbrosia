c569e6c23d8acd5317f3b352dbdd975fe86a2d25
weijchen0919 <weijchen0919@gmail.com>
2021-09-25 20:05:54 -0400

    add fog and gamemanager prefabs

--- Assets/FogBall.cs
+++ Assets/FogBall.cs
@@ -10,15 +10,17 @@ public class FogBall : MonoBehaviour
 {
     [SerializeField] private Transform fogCenterPoint;
     [SerializeField] private GameObject player;
-    private MeshRenderer _meshRenderer;
     [SerializeField] private float destroyDist = 0.03f;
+   
+    private Material fogMaterial;
     private bool isPlayerEnter = false;
-    
-    
+    private float radius = 0f;
+
     void Start()
     {
-        _meshRenderer = GetComponent<MeshRenderer>();
+        fogMaterial = GetComponent<MeshRenderer>().material;
     }
+    
 
     void Update()
     {
@@ -33,6 +35,7 @@ public class FogBall : MonoBehaviour
         if (other.transform.tag == "Player")
         {
             isPlayerEnter = true;
+            radius = Vector3.Distance(other.transform.position, transform.position);
         }
     }
 
@@ -47,14 +50,12 @@ public class FogBall : MonoBehaviour
     private void AdjustAlpha()
     {
         float dist = Vector3.Distance(player.transform.position , fogCenterPoint.transform.position);
-        Color color = _meshRenderer.material.color;
-        Debug.Log(dist);
-        Debug.Log(color.a);
+        
         if (dist > destroyDist)
         {
-            float alpha = Mathf.Min(_meshRenderer.material.color.a, dist / destroyDist);
-            color.a = alpha;
-            _meshRenderer.material.color = color;
+            Color color = fogMaterial.color;
+            color.a = dist / radius;
+            fogMaterial.color = color;
         }
         else
         {

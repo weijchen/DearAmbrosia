76df3621193b2a3074039ceb7aed39893c388669
sx18014 <1229918014@qq.com>
2021-09-27 16:18:39 -0400

    add EndingTrigger 2.0

--- Assets/Scripts/EndingTrigger.cs
+++ Assets/Scripts/EndingTrigger.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EndingTrigger : MonoBehaviour
 {
+    public GameObject LoveLetterCube;
+    public GameObject ChocolateCube;
+    public GameObject RoseCube;
+
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _HEClip;
+    [SerializeField] private AudioClip _BEClip;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,15 +28,21 @@ public class EndingTrigger : MonoBehaviour
     {
         if (other.gameObject.tag == "LoveLetter")
         {
-            Debug.Log(100);
+            Debug.Log("LoveLetter Received");
+            LoveLetterCube.SetActive(false);
+            _audioSource.PlayOneShot(_HEClip);
         }
         else if (other.gameObject.tag == "Chocolate")
         {
-            Debug.Log(200);
+            Debug.Log("Chocolate Received");
+            ChocolateCube.SetActive(false);
+            _audioSource.PlayOneShot(_BEClip);
         }
         else if (other.gameObject.tag == "Rose")
         {
-            Debug.Log(300);
+            Debug.Log("Rose Received");
+            RoseCube.SetActive(false);
+            _audioSource.PlayOneShot(_HEClip);
         }
     }
 }
